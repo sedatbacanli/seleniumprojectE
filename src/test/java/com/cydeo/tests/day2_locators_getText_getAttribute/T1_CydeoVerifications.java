81ccc84ef8e9ff3b6217f9b7d9ca1d48eb8a8ebe
sedatbacanli <sedatbacanli10@gmail.com>
2022-05-06 16:24:26 +0300

    day2 Task1_CydeoVerifications

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
@@ -1,10 +1,18 @@
 package com.cydeo.tests.day2_locators_getText_getAttribute;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class T1_CydeoVerifications {
     public static void main(String[] args) {
 
         //TC #1: Yahoo Title Verification
         //1. Open Chrome browser
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
         //2. Go to https://www.yahoo.com
         //3. Verify title:
         //Expected: Yahoo
