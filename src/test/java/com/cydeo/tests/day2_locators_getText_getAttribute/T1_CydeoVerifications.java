8eac88071bcf138473f336fd5874323dbb59d61f
sedatbacanli <sedatbacanli10@gmail.com>
2022-05-06 16:16:03 +0300

    day2 Task1_CydeoVerifications

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
@@ -0,0 +1,12 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+public class T1_CydeoVerifications {
+    public static void main(String[] args) {
+
+        //TC #1: Yahoo Title Verification
+        //1. Open Chrome browser
+        //2. Go to https://www.yahoo.com
+        //3. Verify title:
+        //Expected: Yahoo
+    }
+}
