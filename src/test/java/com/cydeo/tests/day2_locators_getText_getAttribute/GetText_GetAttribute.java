a9dc2be6e747e67b0df537d4ed6da0c411ad150b
sedatbacanli <sedatbacanli10@gmail.com>
2022-05-08 18:46:50 +0300

    day2 Task5_GetText_GetAttribute

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/GetText_GetAttribute.java
@@ -0,0 +1,26 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GetText_GetAttribute {
+    public static void main(String[] args) {
+        //
+        //TC #5: getText() and getAttribute() method practice
+        //1- Open a chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //2- Go to: https://practice.cydeo.com/registration_form
+        driver.get("https://practice.cydeo.com/registration_form");
+
+        //3- Verify header text is as expected:
+        //  Expected: Registration form
+
+        //4- Locate “First name” input box
+        //5- Verify placeholder attribute’s value is as expected:
+        //  Expected: first name
+    }
+}
