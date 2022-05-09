ae67962d06e967039fc7886ffffb05bf74a029cb
sedatbacanli <sedatbacanli10@gmail.com>
2022-05-09 18:56:41 +0300

    day2 HomeWork_4

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/HomeWork_4.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/HomeWork_4.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day2_locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HomeWork_4 {
@@ -17,9 +19,20 @@ public class HomeWork_4 {
         //2- Go to: https://practice.cydeo.com/inputs
         driver.get("https://practice.cydeo.com/inputs ");
         //3- Click to “Home” link
+        WebElement Home = driver.findElement(By.linkText("Home"));
+        Home.click();
 
         //4- Verify title is as expected:
         //Expected: Practice
+        String expecedTtle = "Practice";
+        String actualTitle = driver.getTitle();
+        if (expecedTtle.equals(actualTitle)) {
+            System.out.println("Title Verification is PASSED!!!");
+        }else {
+            System.out.println("Title Verification is FAILED!!!");
+        }
+
+
         //
         //PS: Locate “Home” link using “className” locator
         //
