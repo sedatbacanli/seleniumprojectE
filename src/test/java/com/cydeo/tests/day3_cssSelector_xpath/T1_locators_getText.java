b1b99a8d87502ba45d7d50b536d477ec9683b309
sedatbacanli <sedatbacanli10@gmail.com>
2022-05-09 20:44:53 +0300

    day3 Task1_locators_getText

---
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T1_locators_getText.java
@@ -0,0 +1,18 @@
+package com.cydeo.tests.day3_cssSelector_xpath;
+
+public class T1_locators_getText {
+    public static void main(String[] args) {
+        //
+        //TC #1: NextBaseCRM, locators and getText() practice
+        //1- Open a chrome browser
+        //2- Go to: https://login1.nextbasecrm.com/
+        //3- Enter incorrect username: “incorrect”
+        //4- Enter incorrect password: “incorrect”
+        //5- Click to login button.
+        //6- Verify error message text is as expected:
+        //Expected: Incorrect login or password
+
+        //PS: Inspect and decide which locator you should be using to locate web
+        //elements.
+    }
+}
