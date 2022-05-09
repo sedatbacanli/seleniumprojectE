0a9713768ee61f047a4025a532f6dbc045a8c612
sedatbacanli <sedatbacanli10@gmail.com>
2022-05-09 17:41:59 +0300

    day2 HomeWork_2

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/HomeWork_2.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/HomeWork_2.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day2_locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HomeWork_2 {
@@ -17,5 +19,13 @@ public class HomeWork_2 {
 
         //3. Verify header text
         //Expected: “Log in to ZeroBank”
+        WebElement headerText = driver.findElement(By.tagName("h3"));
+        String expectedHeaderText = "Log in to ZeroBank";
+        String actualHeaderText = headerText.getText();
+        if (expectedHeaderText.equals(actualHeaderText)) {
+            System.out.println("Header Text Verification PASSED!!!");
+        }else {
+            System.out.println("Header Text Verification FAILED!!!");
+        }
     }
 }
