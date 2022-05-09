1cd52276303f41322bb0ba42fca35f00c1c2151e
sedatbacanli <sedatbacanli10@gmail.com>
2022-05-09 17:22:42 +0300

    day2 HomeWork_2

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/HomeWork_2.java
@@ -0,0 +1,21 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomeWork_2 {
+    public static void main(String[] args) {
+        //TC #2: Zero Bank header verification
+        //1. Open Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //2. Go to http://zero.webappsecurity.com/login.html
+        driver.get("http://zero.webappsecurity.com/login.html");
+
+        //3. Verify header text
+        //Expected: “Log in to ZeroBank”
+    }
+}
