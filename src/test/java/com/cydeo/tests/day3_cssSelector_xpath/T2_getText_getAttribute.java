373888f0051b1d65e0c0c56806ea56fbb4fe0d13
sedatbacanli <sedatbacanli10@gmail.com>
2022-05-10 19:37:39 +0300

    day3 T2_getText_getAttribute

--- src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T2_getText_getAttribute.java
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T2_getText_getAttribute.java
@@ -1,21 +1,30 @@
 package com.cydeo.tests.day3_cssSelector_xpath;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
 
 public class T2_getText_getAttribute {
     public static void main(String[] args) {
         //TC #2: NextBaseCRM, locators, getText(), getAttribute() practice
         //1- Open a chrome browser
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+       WebDriver driver = WebDriverFactory.getDriver("Chrome");
         driver.manage().window().maximize();
 
     //2- Go to: https://login1.nextbasecrm.com/
         driver.get("https://login1.nextbasecrm.com/");
+
         //3- Verify “remember me” label text is as expected:
         //Expected: Remember me on this computer
+        WebElement rememberMeLabel = driver.findElement(By.className("login-item-checkbox-label"));
+        String expectedRememberMeLabel = "Remember me on this computer";
+        String actualRememberMeLabel = rememberMeLabel.getText();
+        if (expectedRememberMeLabel.equals(actualRememberMeLabel)){
+            System.out.println("Label Verification is PASSED!!");
+        }else{
+            System.out.println("Label Verification is FAILED!!!");
+        }
         //4- Verify “forgot password” link text is as expected:
         //Expected: Forgot your password?
         //5- Verify “forgot password” href attribute’s value contains expected:
