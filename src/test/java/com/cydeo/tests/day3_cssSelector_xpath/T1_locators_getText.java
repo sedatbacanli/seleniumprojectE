340d2943af936d0e7129032f6de9dc978852955d
sedatbacanli <sedatbacanli10@gmail.com>
2022-05-10 15:38:59 +0300

    day3 Task1_locators_getText

--- src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T1_locators_getText.java
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T1_locators_getText.java
@@ -1,12 +1,24 @@
 package com.cydeo.tests.day3_cssSelector_xpath;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class T1_locators_getText {
     public static void main(String[] args) {
         //
         //TC #1: NextBaseCRM, locators and getText() practice
         //1- Open a chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
         //2- Go to: https://login1.nextbasecrm.com/
+
+        driver.get("https://login1.nextbasecrm.com/");
+
         //3- Enter incorrect username: “incorrect”
+
         //4- Enter incorrect password: “incorrect”
         //5- Click to login button.
         //6- Verify error message text is as expected:
