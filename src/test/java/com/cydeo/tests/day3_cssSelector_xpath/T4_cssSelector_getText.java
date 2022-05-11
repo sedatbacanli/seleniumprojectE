277be30dfd02f8234ccdecdd2aefcbe59c5302e6
sedatbacanli <sedatbacanli10@gmail.com>
2022-05-11 20:38:33 +0300

    day3 T4_cssSelector_getText

---
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T4_cssSelector_getText.java
@@ -0,0 +1,46 @@
+package com.cydeo.tests.day3_cssSelector_xpath;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class T4_cssSelector_getText {
+    public static void main(String[] args) {
+        //TC #4: NextBaseCRM, locators, getText(), getAttribute() practice
+        //1- Open a chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
+        //2- Go to: https://login1.nextbasecrm.com/?forgot_password=yes
+        driver.get("https://login1.nextbasecrm.com/?forgot_password=yes");
+
+        //3- Verify “Reset password” button text is as expected:
+        //Expected: Reset password
+
+        // syntax = tagNAME[attribute = 'value']
+
+        //Locating reset password button using class attribute and its value
+        //WebElement resetPasswordButton = driver.findElement(By.cssSelector("button[class='login-btn']"));
+
+        //Locating reset password button using value attribute and its value
+
+        WebElement resetPasswordButton = driver.findElement(By.cssSelector("button[value = 'Reset password']"));
+
+        String expectedResetPasswordButton = "Reset password";
+        String actualResetPasswordButton = resetPasswordButton.getAttribute("value");
+
+        if (expectedResetPasswordButton.equals(actualResetPasswordButton)){
+            System.out.println("Reset Password Button Verification is PASSED!!!");
+        }else {
+            System.out.println("Reset Password Button Verification is FAILED!!!");
+        }
+
+        //
+        //
+        //PS: Inspect and decide which locator you should be using to locate web
+        //elements.
+        //PS2: Pay attention to where to get the text of this button from
+
+    }
+}
