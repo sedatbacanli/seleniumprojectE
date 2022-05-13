e8afd43b033614d6863f63d3a73799afca95611c
sedatbacanli <sedatbacanli10@gmail.com>
2022-05-13 18:42:26 +0300

    day4 T1_xpath_cssSelector_practice

---
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T1_xpath_cssSelector_practice.java
@@ -0,0 +1,44 @@
+package com.cydeo.tests.day4_findElements_checkboxes_radio;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class T1_xpath_cssSelector_practice {
+    public static void main(String[] args) {
+        //USE ANY DEVELOPER TOOLS TO GET ANY LOCATORS.
+        //TC #1: XPATH and cssSelector Practices
+        //1. Open Chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
+        //2. Go to https://practice.cydeo.com/forgot_password
+        driver.get("https://practice.cydeo.com/forgot_password");
+
+        //3. Locate all the WebElements on the page using XPATH and/or CSS
+        //locator only (total of 6)
+        //a. “Home” link
+        WebElement homeLink_ex1 = driver.findElement(By.cssSelector("a[class='nav-link']"));
+
+        WebElement homeLink_ex2 = driver.findElement(By.cssSelector("a.nav-link"));
+
+        WebElement homeLink_ex3 = driver.findElement(By.cssSelector("a[href='/']"));
+
+
+
+
+        //b. “Forgot password” header
+        //c. “E-mail” text
+        //d. E-mail input box
+        //e. “Retrieve password” button
+        //f. “Powered by Cydeo text
+        //4. Verify all web elements are displayed.
+        //First solve the task with using cssSelector only. Try to create 2 different
+        //cssSelector if possible
+        //Then solve the task using XPATH only. Try to create 2 different
+        //XPATH locator if possible
+
+    }
+
+}
