3252f6ce3218e8dc198fa36541b0f569cf1d4829
sedatbacanli <sedatbacanli10@gmail.com>
2022-05-15 00:04:42 +0300

    day4 T6 StaleElementReferenceException

--- src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T6_StaleElementReferenceException.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T6_StaleElementReferenceException.java
@@ -1,7 +1,29 @@
 package com.cydeo.tests.day4_findElements_checkboxes_radio;
 
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class T6_StaleElementReferenceException {
     public static void main(String[] args) {
+        //TC #6: StaleElementReferenceException Task
+        //1- Open a chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
+        //2- Go to: https://practice.cydeo.com/abtest
+        driver.get("https://practice.cydeo.com/abtest");
+
+        //3- Locate “CYDEO” link, verify it is displayed.
+        WebElement cydeoLink = driver.findElement(By.xpath("//a[text()='CYDEO']"));
+        System.out.println("cydeoLink.isDisplayed() = " + cydeoLink.isDisplayed());
+
+        //4- Refresh the page.
+       // WebElement refreshThePage = driver.findElement(By.xpath(""));
+        driver.navigate().refresh();
+
+        //5- Verify it is displayed, again.
 
     }
 }
