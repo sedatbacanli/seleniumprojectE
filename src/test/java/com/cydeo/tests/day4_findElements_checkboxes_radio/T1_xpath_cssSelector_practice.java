9332478623e2a5ce6da1fb70169f4a45196ea448
sedatbacanli <sedatbacanli10@gmail.com>
2022-05-14 18:54:36 +0300

    day4 T1_xpath_cssSelector_practice

--- src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T1_xpath_cssSelector_practice.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T1_xpath_cssSelector_practice.java
@@ -30,15 +30,36 @@ public class T1_xpath_cssSelector_practice {
         WebElement header_ex1 = driver.findElement(By.cssSelector("div.example>h2"));
 
         //Locate header using xpath
+        // WebElement header_ex2 = driver.findElement(By.xpath("//h2[.='Forgot Password']"));
         WebElement header_ex2 = driver.findElement(By.xpath("//h2[text()='Forgot Password']"));
 
 
 
         //c. “E-mail” text
+        WebElement emailLabel = driver.findElement(By.xpath("//label[@for='email']"));
+
         //d. E-mail input box
+        WebElement inputBox_ex1 = driver.findElement(By.xpath("//input[@name='email']"));
+        WebElement inputBox_ex2 = driver.findElement(By.xpath("//input[contains(@pattern,'a-z')]"));
+
+
+
         //e. “Retrieve password” button
+        WebElement retrievePasswordButton = driver.findElement(By.xpath("//button[@id='form_submit']"));
+
         //f. “Powered by Cydeo text
+        WebElement poweredByCydeoText = driver.findElement(By.xpath("//div[@style='text-align: center;']"));
+
+
         //4. Verify all web elements are displayed.
+
+        System.out.println("header_ex1.isDisplayed() = " + header_ex1.isDisplayed());
+        System.out.println("homeLink_ex1.isDisplayed() = " + homeLink_ex1.isDisplayed());
+        System.out.println("emailLabel.isDisplayed() = " + emailLabel.isDisplayed());
+        System.out.println("inputBox_ex1.isDisplayed() = " + inputBox_ex1.isDisplayed());
+        System.out.println("retrievePasswordButton.isDisplayed() = " + retrievePasswordButton.isDisplayed());
+        System.out.println("poweredByCydeoText.isDisplayed() = " + poweredByCydeoText.isDisplayed());
+
         //First solve the task with using cssSelector only. Try to create 2 different
         //cssSelector if possible
         //Then solve the task using XPATH only. Try to create 2 different
