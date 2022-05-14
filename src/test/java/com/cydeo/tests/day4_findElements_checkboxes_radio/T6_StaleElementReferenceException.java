2fb51b77f41ac06aaab7237b76152a972435b752
sedatbacanli <sedatbacanli10@gmail.com>
2022-05-14 23:40:44 +0300

    day4 T6 StaleElementReferenceException

--- src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T6_StaleElementReferenceException.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T6_StaleElementReferenceException.java
@@ -1,6 +1,6 @@
 package com.cydeo.tests.day4_findElements_checkboxes_radio;
 
-public class StaleElementReferenceException {
+public class T6_StaleElementReferenceException {
     public static void main(String[] args) {
 
     }
