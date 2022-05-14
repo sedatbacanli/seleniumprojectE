c479e1040f86a9999c36b29820e9dd61da7ac868
sedatbacanli <sedatbacanli10@gmail.com>
2022-05-14 21:37:38 +0300

    day4 T5 checkboxes

---
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T5_checkboxes.java
@@ -0,0 +1,16 @@
+package com.cydeo.tests.day4_findElements_checkboxes_radio;
+
+public class T5_checkboxes {
+    public static void main(String[] args) {
+        //
+        //Practice: Checkboxes
+        //1. Go to http://practice.cydeo.com/checkboxes
+        //2. Confirm checkbox #1 is NOT selected by default
+        //3. Confirm checkbox #2 is SELECTED by default.
+        //4. Click checkbox #1 to select it.
+        //5. Click checkbox #2 to deselect it.
+        //6. Confirm checkbox #1 is SELECTED.
+        //7. Confirm checkbox #2 is NOT selected.
+        //
+    }
+}
