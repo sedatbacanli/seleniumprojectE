beae95bf6e89f315b675e4da2ba74bc889206eca
sedatbacanli <sedatbacanli10@gmail.com>
2022-05-17 15:57:59 +0300

    day5 T2 RadioButton

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T2_RadioButton.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T2_RadioButton.java
@@ -1,7 +1,36 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.concurrent.TimeUnit;
+
 public class T2_RadioButton {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        //TC #2: Radiobutton handling
+        //1. Open Chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //2. Go to https://practice.cydeo.com/radio_buttons
+        driver.get("https://practice.cydeo.com/radio_buttons ");
+
+        //3. Click to “Hockey” radio button
+        WebElement hockeyBtn = driver.findElement(By.xpath("//input[@id='hockey']"));
+        Thread.sleep(2000);
+        hockeyBtn.click();
+
+        //4. Verify “Hockey” radio button is selected after clicking.
+        if (hockeyBtn.isSelected()){
+            System.out.println("Hockey Button is selected, Verification is PASSED!");
+        }else {
+            System.out.println("Hockey Button is not selected, Verification is FAİLED!!!");
+        }
+
+        //USE XPATH LOCATOR FOR ALL WEBELEMENT LOCATORS
 
     }
 }
