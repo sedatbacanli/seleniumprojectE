a354d2877e424a6ee8a8c818c5646224b02e8ef7
sedatbacanli <sedatbacanli10@gmail.com>
2022-05-20 18:57:18 +0300

    day5 TestNG_Intro

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -1,10 +1,16 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class TestNG_Intro {
+    @BeforeClass
+    public void setUpMethod(){
+        System.out.println("---> BeforeClassMethod is running!");
+    }
+    @AfterClass
+    public void tearDownMethod(){
+        System.out.println("---> AfterClassMethod is running!");
+    }
 
     @BeforeMethod
     public void setUpMethod(){
