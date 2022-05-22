934d4cb88d44bc3e5b0248d4200de7703279dfce
sedatbacanli <sedatbacanli10@gmail.com>
2022-05-22 15:43:07 +0300

    day5 T4_SimpleDropdowns

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T4_SimpleDropdowns.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T4_SimpleDropdowns.java
@@ -2,13 +2,14 @@ package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class T4_SimpleDropdowns {
-    @Test
-    public void simpleDropdownTest(){
+    @BeforeMethod
+    public void setupMethod(){
         //TC#4: Verifying “Simple dropdown” and “State selection” dropdown
         //default values
         //1. Open Chrome browser
@@ -18,6 +19,12 @@ public class T4_SimpleDropdowns {
 
         //2. Go to https://practice.cydeo.com/dropdown
         driver.get("https://practice.cydeo.com/dropdown");
+
+    }
+
+    @Test
+    public void simpleDropdownTest(){
+
         //3. Verify “Simple dropdown” default selected value is correct
         //Expected: “Please select an option”
         //4. Verify “State selection” default selected value is correct
