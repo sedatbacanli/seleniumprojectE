abb16ae68b0a5cee3e0b9ca2e81881053405d90e
sedatbacanli <sedatbacanli10@gmail.com>
2022-05-22 15:36:35 +0300

    day5 T4_SimpleDropdowns

---
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T4_SimpleDropdowns.java
@@ -0,0 +1,26 @@
+package com.cydeo.tests.day5_testNG_intro_dropdowns;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T4_SimpleDropdowns {
+    @Test
+    public void simpleDropdownTest(){
+        //TC#4: Verifying “Simple dropdown” and “State selection” dropdown
+        //default values
+        //1. Open Chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //2. Go to https://practice.cydeo.com/dropdown
+        driver.get("https://practice.cydeo.com/dropdown");
+        //3. Verify “Simple dropdown” default selected value is correct
+        //Expected: “Please select an option”
+        //4. Verify “State selection” default selected value is correct
+        //Expected: “Select a State”
+    }
+}
--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Selenium.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Selenium.java
@@ -2,11 +2,18 @@ package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class TestNG_Selenium {
+
+    @BeforeMethod
+    public void setupMethod(){
+
+    }
     @Test
     public  void  selenium_test(){
 
@@ -14,6 +21,15 @@ public class TestNG_Selenium {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://google.com");
+
+
+        //SAME THING
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Google";
+        Assert.assertEquals(actualTitle,expectedTitle, "Not Matching");
+
+
+        //Assert.assertEquals(driver.getTitle(), "Google");
 
     }
 
