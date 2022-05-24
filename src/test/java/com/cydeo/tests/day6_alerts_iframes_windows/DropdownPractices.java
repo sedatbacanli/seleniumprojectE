848d33b28ea1f098591baf27b30693ef9c3de352
sedatbacanli <sedatbacanli10@gmail.com>
2022-05-24 17:05:07 +0300

    day6 DropdownPractices

---
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
@@ -0,0 +1,13 @@
+package com.cydeo.tests.day6_alerts_iframes_windows;
+
+import org.testng.annotations.Test;
+
+public class DropdownPractices {
+
+    @Test
+
+public void dropdown_task5(){
+
+    }
+
+}
