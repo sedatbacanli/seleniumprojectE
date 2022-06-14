45e99c3f87e5a078b54a387c9f440e19894f622e
sedatbacanli <sedatbacanli10@gmail.com>
2022-06-14 16:40:47 +0300

    day6 T4_IframePractice

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T1_Alert_Practices.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T1_Alert_Practices.java
@@ -1,7 +1,11 @@
 package com.cydeo.tests.day6_alerts_iframes_windows;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -26,6 +30,24 @@ public class T1_Alert_Practices {
     }
     @Test
     public void alert_test1(){
+
+        //3. Click to “Click for JS Alert” button
+        WebElement informationAlertButton = driver.findElement(By.xpath("//button[.= 'Click for JS Alert']"));
+        informationAlertButton.click();
+
+        Alert alert = driver.switchTo().alert();
+        //4. Click to OK button from the alert
+        alert.accept();
+        //5. Verify “You successfully clicked an alert” text is displayed.
+        WebElement resultText = driver.findElement(By.xpath("//p[@id='result']"));
+        Assert.assertTrue(resultText.isDisplayed(), "Result text is NOT displayed");
+
+        String expectedText = "You successfully clicked an alert";
+        String actualText = resultText.getText();
+        Assert.assertEquals(actualText,expectedText, "Result text Not displayed");
+
+
+        //
 
     }
 }
